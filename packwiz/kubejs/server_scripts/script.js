9839968735305a7d15c76ec5e091e94d376aca8c
sitasreturn <87218882+sitasreturn@users.noreply.github.com>
2022-10-16 17:15:23 +0700

    test fix

--- packwiz/kubejs/server_scripts/script.js
+++ packwiz/kubejs/server_scripts/script.js
@@ -6,61 +6,74 @@ settings.logSkippedRecipes = false;
 settings.logErroringRecipes = true;
 
 console.info(
-	'Hello, World! (You will see this line every time server resources reload)'
+  "Hello, World! (You will see this line every time server resources reload)"
 );
 
 ServerEvents.recipes((event) => {
-	// Change recipes here
+	
+  //QoL stuff
 
-	//TODO: This is a test recipe
-	event.remove({ output: 'minecraft:stone' });
+  event.shaped("minecraft:stone", ["CF ", "   ", "   "], {
+    C: "minecraft:cobblestone",
+    F: "minecraft:fire_charge",
+  });
 
-	event.shaped('minecraft:stone', ['CF ', '   ', '   '], {
-		C: 'minecraft:cobblestone',
-		F: 'minecraft:fire_charge',
-	});
+  // Change recipes here
 
-	event.custom({
-		type: 'modern_industrialization:assembler',
-		eu: 32,
-		duration: 1000,
-		item_inputs: [
-			{
-				item: 'modern_industrialization:highly_advanced_upgrade',
-				amount: 32,
-			},
-			{
-				item: 'modern_industrialization:quantum_circuit',
-				amount: 8,
-			},
-			{
-				item: 'modern_industrialization:singularity',
-				amount: 1,
-			},
-			{
-				item: 'modern_industrialization:quantum_circuit_board',
-				amount: 1,
-			},
-		],
+  // Modern Industrialization
 
-		fluid_inputs: {
-			fluid: 'modern_industrialization:uu_matter',
-			amount: 50,
-		},
-		item_outputs: [
-			{
-				item: 'modern_industrialization:quantum_upgrade',
-				amount: 1,
-			},
-		],
-	});
+  // Quantum Stuff
+
+  event.remove({ output: "modern_industrialization:quantum_upgrade" });
+  event.custom({
+    type: "modern_industrialization:assembler",
+    eu: 32,
+    duration: 1000,
+    item_inputs: [
+      {
+        item: "modern_industrialization:highly_advanced_upgrade",
+        amount: 32,
+      },
+      {
+        item: "modern_industrialization:quantum_circuit",
+        amount: 8,
+      },
+      {
+        item: "modern_industrialization:singularity",
+        amount: 4,
+      },
+      {
+        item: "modern_industrialization:quantum_circuit_board",
+        amount: 1,
+      },
+      {
+        item: "botania:gaia_ingot",
+        amount: 8,
+      },
+      {
+        item: "ae2:singularity",
+        amount: 32,
+      },
+    ],
+
+    fluid_inputs: {
+      fluid: "modern_industrialization:uu_matter",
+      amount: 50,
+    },
+    item_outputs: [
+      {
+        item: "modern_industrialization:quantum_upgrade",
+        amount: 1,
+      },
+    ],
+  });
 });
 
-ServerEvents.tags('item', (event) => {
-	// Change tags here
-	// Examples:
-	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
-	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
-	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
-	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+ServerEvents.tags("item", (event) => {
+  // Change tags here
+  // Examples:
+  // Get the #forge:cobblestone tag collection and add Diamond Ore to it
+  // event.get('forge:cobblestone').add('minecraft:diamond_ore')
+  // Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
+  // event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
 });
