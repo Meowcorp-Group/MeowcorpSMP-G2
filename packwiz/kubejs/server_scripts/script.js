b2aab2c1aa453d39786925be83d286dea7cc2cf4
sitasreturn <87218882+sitasreturn@users.noreply.github.com>
2022-10-16 15:15:21 +0700

    assembler recipe test

--- packwiz/kubejs/server_scripts/script.js
+++ packwiz/kubejs/server_scripts/script.js
@@ -5,27 +5,59 @@ settings.logRemovedRecipes = true;
 settings.logSkippedRecipes = false;
 settings.logErroringRecipes = true;
 
-console.info('Hello, World! (You will see this line every time server resources reload)');
+console.info(
+	'Hello, World! (You will see this line every time server resources reload)'
+);
 
 ServerEvents.recipes((event) => {
 	// Change recipes here
-	
+
 	//TODO: This is a test recipe
 	event.remove({ output: 'minecraft:stone' });
 
-	event.shaped('minecraft:stone', [
-		'CF ',
-		'   ',
-		'   '
-	], {
+	event.shaped('minecraft:stone', ['CF ', '   ', '   '], {
 		C: 'minecraft:cobblestone',
-		F: 'minecraft:fire_charge'
+		F: 'minecraft:fire_charge',
+	});
+
+	event.custom({
+		type: 'modern_industrialization:assembler',
+		eu: 32,
+		duration: 1000,
+		item_inputs: [
+			{
+				item: 'modern_industrialization:highly_advanced_upgrade',
+				amount: 32,
+			},
+			{
+				item: 'modern_industrialization:quantum_circuit',
+				amount: 8,
+			},
+			{
+				item: 'modern_industrialization:singularity',
+				amount: 1,
+			},
+			{
+				item: 'modern_industrialization:quantum_circuit_board',
+				amount: 1,
+			},
+		],
+
+		fluid_inputs: {
+			fluid: 'modern_industrialization:uu_matter',
+			amount: 50,
+		},
+		item_outputs: [
+			{
+				item: 'modern_industrialization:quantum_upgrade',
+				amount: 1,
+			},
+		],
 	});
 });
 
 ServerEvents.tags('item', (event) => {
 	// Change tags here
-
 	// Examples:
 	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
 	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
