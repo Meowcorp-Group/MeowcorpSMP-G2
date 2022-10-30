92cfc71120925aa206d5aa636a893231c528a80c
Waoweens <30313089+Waoweens@users.noreply.github.com>
2022-10-30 22:48:01 +0700

    Update mods, remove Colormatic and mem leak fix for kubejs

--- packwiz/kubejs/server_scripts/script.js
+++ packwiz/kubejs/server_scripts/script.js
@@ -1,1 +1,15 @@
-// beta 1.19 kubejs doesn't work with MI and TR :I
+// priority: 0
+
+console.info('Hello, World! (You will see this line every time server resources reload)')
+
+ServerEvents.recipes(event => {
+	// Change recipes here
+})
+
+ServerEvents.tags('item', event => {
+	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
+	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
+
+	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
+	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+})
