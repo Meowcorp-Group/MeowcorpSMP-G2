d0b5c1ea5633761c0c4c49fcbaf7aef4e31d0fb9
sitasreturn <87218882+sitasreturn@users.noreply.github.com>
2022-11-01 19:36:57 +0700

    custom recipe test

--- packwiz/kubejs/server_scripts/script.js
+++ packwiz/kubejs/server_scripts/script.js
@@ -9,6 +9,49 @@ ServerEvents.recipes((event) => {
   // Change recipes here
 
   event.remove({output: "modern_industrialization:quantum_upgrade"});
+  event.custom({
+    type: "modern_industrialization:assembler",
+    eu: 32,
+    duration: 1000,
+    item_inputs: [
+      {
+        item: "modern_industrialization:highly_advanced_upgrade",
+        amount: 32,
+      },
+      {
+        item: "modern_industrialization:quantum_circuit",
+        amount: 8,
+      },
+      {
+        item: "modern_industrialization:singularity",
+        amount: 4,
+      },
+      {
+        item: "modern_industrialization:quantum_circuit_board",
+        amount: 1,
+      },
+      {
+        item: "botania:gaia_ingot",
+        amount: 8,
+      },
+      {
+        item: "ae2:singularity",
+        amount: 32,
+      },
+    ],
+
+    fluid_inputs: {
+      fluid: "modern_industrialization:uu_matter",
+      amount: 50,
+    },
+    item_outputs: [
+      {
+        item: "modern_industrialization:quantum_upgrade",
+        amount: 1,
+      },
+    ],
+  });
+
 
   // Add recipes here
 
@@ -16,8 +59,9 @@ ServerEvents.recipes((event) => {
     C: "minecraft:cobblestone",
     F: "minecraft:fire_charge",
 
-  });
-});
+  })
+})
+
 
 ServerEvents.tags("item", (event) => {
   // Get the #forge:cobblestone tag collection and add Diamond Ore to it
