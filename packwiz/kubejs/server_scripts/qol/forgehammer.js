670e83fcea6e6c8678a3f4e4fce980db83461c70
sitasreturn <87218882+sitasreturn@users.noreply.github.com>
2023-01-04 08:24:26 +0700

    test

---
+++ packwiz/kubejs/server_scripts/qol/forgehammer.js
@@ -0,0 +1,22 @@
+ServerEvents.recipes((event) => {
+    
+    event.custom(
+        {
+        type: "modern_industrialization:forge_hammer",
+        duration: 0,
+        eu: 30,
+        item_inputs: [
+          {
+            amount: 3,
+            tag: "c:raw_zinc_ores"
+          }
+        ],
+        item_outputs: [
+          {
+            amount: 4,
+            item: "techreborn:zinc_dust"
+          }
+        ]
+      });
+
+})
